5839c5e9d494f73b2e589e6a0f8a223d78530c7c
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-12-05 21:53:40 -0600

    rewrite fetching data structure and useEffect hook usage

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -22,29 +22,22 @@ const Record = (props) => (
 export default function RecordList() {
 	const [records, setRecords] = useState([]);
 
-	// This method fetches the records from the database.
 	useEffect(() => {
-		async function getRecords() {
-			const response = await fetch(`http://localhost:8080/record/`);
-				if (!response.ok) {
-					const message = `An error occurred: ${response.statusText}`;
-					window.alert(message);
-					return;
-			}
-			const records = await response.json();
-			setRecords(records);
-		}
-		getRecords();
-		return;
-	}, [records.length]);
+		getEmployeeRecords();
+	}, [])
+
+	function getEmployeeRecords() {
+		fetch('http://localhost:8080/record/')
+		.then(response => response.json())
+		.then(data => setRecords(data))
+		.catch(error => console.error('Error:', error));
+	}
 
 	// This method will delete a record
-	async function deleteRecord(id) {
-		await fetch(`http://localhost:8080/${id}`, {
-			method: "DELETE"
-		});
-		const newRecords = records.filter((el) => el._id !== id);
-		setRecords(newRecords);
+	function deleteRecord(id) {
+		fetch(`http://localhost:8080/${id}`, {method: "DELETE"})
+		.then(_ => getEmployeeRecords())
+		.catch(error => console.error('Error:', error));
 	}
 
 	// This method will map out the records on the table
